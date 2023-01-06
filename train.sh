301e7d3feb9bd8c246a08d3d96cf258c29fb44e4
TCL <1620508360@qq.com>
2023-01-06 20:34:49 +0800

    chore: rm train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /root/bqqi/changli/STD2022
-
-python3 debug.py  --train \
-                --test_dir /root/bqqi/changli/Test/Clean \
-                --valid_npy /root/bqqi/changli/STD2022/data/valid_334.npy \
-                --gpu
